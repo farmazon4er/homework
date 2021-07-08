bdaf53c525a522782908f2d79e006e5bc81e3d91
farmazon4er <79212484+farmazon4er@users.noreply.github.com>
2021-07-08 12:28:32 +0500

    Update and rename les2_pokermon.rb.txt to les2_pokermon.rb

---
+++ les2_pokermon.rb
@@ -0,0 +1,15 @@
+def pokemon_input(n, pok_list)
+(n).times {|i|
+  puts "Введите имя #{i+1} покемона"
+  name = gets.chomp.to_s
+  puts "Введите цвет #{i+1} покемона"
+  color = gets.chomp.to_s
+  pok_list[name] = color
+    }
+end
+
+puts "Введите количество покемонов"
+n = gets.chomp.to_i
+pok_list = Hash.new
+pokemon_input(n, pok_list)
+pok_list.each {|name, color| puts "У покемона #{name} цвет - #{color}" }
